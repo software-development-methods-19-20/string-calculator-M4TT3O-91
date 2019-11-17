e03beb279e3aaabe70c048396e186a9d97306c34
Matteo <teo_exc@hotmail.it>
2019-11-17 17:07:23 +0100

    - Add support for new Line in string (improved)

--- production/source/dssc/StringCalcualtor.java
+++ production/source/dssc/StringCalcualtor.java
@@ -2,6 +2,7 @@ package dssc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -15,28 +16,14 @@ public class StringCalcualtor {
     }
 
     public static int StringAdder(String numbers){
-        int sum=0;
-        String delimiter ="[,\n]+";
-        String[] token = numbers.split(delimiter);
+        Stream<String> st =Arrays.asList(numbers).stream();
+        String delimiter = "[,\n]+";
+        Stream<String> list=Arrays.stream(numbers.split(delimiter));
 
-        return Arrays.stream(numbers.split(delimiter)).map(x->Integer.parseInt(x)).reduce(0,(x,y)->x+y);
-
-        /*for (String i : token)
-            sum =sum+Integer.valueOf(i);
-        return sum;*/
+        return list.map(Integer::parseInt).reduce(0,(x, y)->x+y);
+        //return Arrays.stream(numbers.split(delimiter)).map(Integer::parseInt).reduce(0,(x, y)->x+y);
 
     }
+    /*subSequence(int beginIndex, int endIndex)*/
 
 }
-
-
-
-
-
-/*
-String s = "prefix/dir1/dir2/dir3/dir4";
-String[] tokens = s.split("/");
-
-for (String t : tokens)
-  System.out.println(t);
-* */
--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import dssc.StringCalcualtor;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -16,15 +17,19 @@ public class AddNumbersTest {
 
     @Test
     void oneNumber() { assertThat(StringCalcualtor.add("1"), is(1)); }
-
     @Test
-    void twoNumbers() {
-        assertThat(StringCalcualtor.add("1,2"),is(3));
-    }
-
+    void twoNumbers() { assertThat(StringCalcualtor.add("1,2"),is(3)); }
     @Test
     void numbers(){assertThat(StringCalcualtor.add("1,2,3"), is(6)); }
     @Test
-    void NewLine (){assertThat(StringCalcualtor.add("1\n2\n3"),is(6));}
+    void NewLine (){assertThat(StringCalcualtor.add("1,2\n3"),is(6));}
 
+    /*Support different delimiters.
+    To change a delimiter, the beginning of the string will contain a separate line
+    that looks like this:
+    “//[delimiter]\n[numbers…]”. For example, “//;\n1;2”
+    should return 3 where the default delimiter is ;.
+    The first line is optional. All existing scenarios should still be supported.*/
+    @Test
+    void InsertDelimiter (){assertThat(StringCalcualtor.add("//;\n1;2"),is(3));}
 }
